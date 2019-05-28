5a82df912df4ca6f5e591230aecc18dc16b86771
Mateusz Kuźniarek <210245@edu.p.lodz.pl>
2019-05-28 15:42:38 +0200

    Started defining membership functions

--- src/main/java/logic/membershipfunction/TrapezoidFunction.java
+++ src/main/java/logic/membershipfunction/TrapezoidFunction.java
@@ -11,9 +11,9 @@ public class TrapezoidFunction implements MembershipFunction {
     private double a1, b1, b2, a2;
 
     public double calculateMembership(double attributeValue) {
-        if (attributeValue <= a1) { return 0; }
+        if (attributeValue >= b1 && attributeValue <= b2) { return 1; }
+        else if (attributeValue <= a1) { return 0; }
         else if (attributeValue >= a2) { return 0; }
-        else if (attributeValue >= b1 && attributeValue <= b2) { return 1; }
         else if (attributeValue < b1) { return (1 / (b1 - a1)) * (attributeValue - a1); }
         else { return (1 / (b2 - a2)) * (attributeValue - a2); }
     }
